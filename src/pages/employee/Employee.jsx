cce771f394565d1a23e2646b5b6a2852cba0c1a1
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-12-01 10:40:35 +0100

    test tans table package

--- src/pages/employee/Employee.jsx
+++ src/pages/employee/Employee.jsx
@@ -1,94 +1,366 @@
 import './employee.css';
 import { NavLink } from 'react-router-dom';
 
-import DataTable from 'react-data-table-component';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  sortingFns,
+  useReactTable
+} from "@tanstack/react-table"
 
-const columns = [
-	{
-		name: 'First Name',
-		selector: row => row.firstName,
-    sortable: true,
-	},
-	{
-		name: 'Last Name',
-		selector: row => row.lastName,
-    sortable: true,
-	},
-  {
-    name: 'Start Date',
-    selector: row => row.startDate,
-    sortable: true,
-  },
-  {
-    name: 'Department',
-    selector: row => row.department,
-    sortable: true,
-  },
-  {
-    name: 'Date of Birth',
-    selector: row => row.dateOfBirth,
-    sortable: true,
-  },
-  {
-    name: 'Street',
-    selector: row => row.street,
-    sortable: true,
-  },
-  {
-    name: 'City',
-    selector: row => row.city,
-    sortable: true,
-  },
-  {
-    name: 'State',
-    selector: row => row.state,
-    sortable: true,
-  },
-  {
-    name: 'Zip Code',
-    selector: row => row.zipCode,
-    sortable: true,
+// A TanStack fork of Kent C. Dodds' match-sorter library that provides ranking information
+import { rankItem, compareItems } from "@tanstack/match-sorter-utils"
+
+import { makeData } from "./makeData"
+
+// Define a custom fuzzy filter function that will apply ranking info to rows (using match-sorter utils)
+const fuzzyFilter = (row, columnId, value, addMeta) => {
+  // Rank the item
+  const itemRank = rankItem(row.getValue(columnId), value)
+
+  // Store the itemRank info
+  addMeta({
+    itemRank
+  })
+
+  // Return if the item should be filtered in/out
+  return itemRank.passed
+}
+
+// Define a custom fuzzy sort function that will sort by rank if the row has ranking information
+const fuzzySort = (rowA, rowB, columnId) => {
+  let dir = 0
+
+  // Only sort by rank if the column has ranking information
+  if (rowA.columnFiltersMeta[columnId]) {
+    dir = compareItems(
+      rowA.columnFiltersMeta[columnId]?.itemRank,
+      rowB.columnFiltersMeta[columnId]?.itemRank
+    )
   }
-];
 
-const data = [
-  {
-    id: 1,
-    firstName: 'Michel',
-    lastName: 'Russel',
-    startDate: '01/01/2020',
-    department: 'Marketing',
-    dateOfBirth: '01/01/1990',
-    street: '123 Main St',
-    city: 'New York',
-    state: 'NY',
-    zipCode: '10001',
-  },
-  {
-    id: 2,
-    firstName: 'Roger',
-    lastName: 'Balkani',
-    startDate: '01/01/2021',
-    department: 'Sales',
-    dateOfBirth: '01/01/1985',
-    street: '456 Main St',
-    city: 'Alabama',
-    state: 'AL',
-    zipCode: '47000',
-  },
-]
+  // Provide an alphanumeric fallback for when the item ranks are equal
+  return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir
+}
+
 
 function Employee() {
+  /* const rerender = React.useReducer(() => ({}), {})[1] */
+
+  const [columnFilters, setColumnFilters] = React.useState([])
+  const [globalFilter, setGlobalFilter] = React.useState("")
+
+  const columns = React.useMemo(
+    () => [
+      /* {
+        accessorKey: "id",
+        filterFn: "equalsString" //note: normal non-fuzzy filter column - exact match required
+      }, */
+      {
+        accessorKey: "firstName",
+        cell: info => info.getValue(),
+        filterFn: "includesStringSensitive" //note: normal non-fuzzy filter column
+      },
+      {
+        accessorFn: row => row.lastName, //note: normal non-fuzzy filter column - case sensitive
+        id: "lastName",
+        cell: info => info.getValue(),
+        header: () => <span>Last Name</span>,
+        filterFn: "includesString" //note: normal non-fuzzy filter column - case insensitive
+      },
+      {
+        accessorKey: "startDate",
+        header: "Start Date",
+        cell: info => info.getValue(),
+      },
+      {
+        accessorKey: "department",
+        header: "Department",
+        cell: info => info.getValue()
+      },
+      { 
+        accessorKey: "dateOfBirth",
+        header: "Date of Birth",
+        cell: info => info.getValue()
+      },
+      {
+        accessorKey: "street",
+        header: "Street",
+        cell: info => info.getValue()
+      },
+      {
+        accessorKey: "city",
+        header: "City",
+        cell: info => info.getValue()
+      },
+      {
+        accessorKey: "state",
+        header: "State",
+        cell: info => info.getValue()
+      },
+      {
+        accessorKey: "zipCode",
+        header: "Zip Code",
+        cell: info => info.getValue()
+      }
+    ],
+    []
+  )
+
+  const [data, setData] = React.useState(() => makeData(5_000))
+  /* const refreshData = () => setData(_old => makeData(50_000)) //stress test */
+
+  const table = useReactTable({
+    data,
+    columns,
+    filterFns: {
+      fuzzy: fuzzyFilter //define as a filter function that can be used in column definitions
+    },
+    state: {
+      columnFilters,
+      globalFilter
+    },
+    onColumnFiltersChange: setColumnFilters,
+    onGlobalFilterChange: setGlobalFilter,
+    globalFilterFn: "fuzzy", //apply fuzzy filter to the global filter (most common use case for fuzzy filter)
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(), //client side filtering
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    debugTable: true,
+    debugHeaders: true,
+    debugColumns: false
+  })
+
+  //apply the fuzzy sort if the fullName column is being filtered
+  React.useEffect(() => {
+    if (table.getState().columnFilters[0]?.id === "fullName") {
+      if (table.getState().sorting[0]?.id !== "fullName") {
+        table.setSorting([{ id: "fullName", desc: false }])
+      }
+    }
+  }, [table.getState().columnFilters[0]?.id])
+
   return (
     <main>
       <h1>Current Employees</h1>
-      <DataTable
-			columns={columns}
-			data={data}
-		  />
+      <NavLink to='/'>Home</NavLink>
+    
+      <div className="p-2">
+        <div>
+          <DebouncedInput
+            value={globalFilter ?? ""}
+            onChange={value => setGlobalFilter(String(value))}
+            className="p-2 font-lg shadow border border-block"
+            placeholder="Search all columns..."
+          />
+        </div>
+        <div className="h-2" />
+        <table>
+          <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
+                  return (
+                    <th key={header.id} colSpan={header.colSpan}>
+                      {header.isPlaceholder ? null : (
+                        <>
+                          <div
+                            {...{
+                              className: header.column.getCanSort()
+                                ? "cursor-pointer select-none"
+                                : "",
+                              onClick: header.column.getToggleSortingHandler()
+                            }}
+                          >
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                            {{
+                              asc: " 🔼",
+                              desc: " 🔽"
+                            }[header.column.getIsSorted()] ?? null}
+                          </div>
+                          {/* {header.column.getCanFilter() ? (
+                            <div>
+                              <Filter column={header.column} />
+                            </div>
+                          ) : null} */}
+                        </>
+                      )}
+                    </th>
+                  )
+                })}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map(row => {
+              return (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map(cell => {
+                    return (
+                      <td key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    )
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        <div className="h-2" />
+        <div className="flex items-center gap-2">
+          <button
+            className="border rounded p-1"
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            {"<<"}
+          </button>
+          <button
+            className="border rounded p-1"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            {"<"}
+          </button>
+          <button
+            className="border rounded p-1"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            {">"}
+          </button>
+          <button
+            className="border rounded p-1"
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            {">>"}
+          </button>
+          <span className="flex items-center gap-1">
+            <div>Page</div>
+            <strong>
+              {table.getState().pagination.pageIndex + 1} of{" "}
+              {table.getPageCount()}
+            </strong>
+          </span>
+          <span className="flex items-center gap-1">
+            | Go to page:
+            <input
+              type="number"
+              defaultValue={table.getState().pagination.pageIndex + 1}
+              onChange={e => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                table.setPageIndex(page)
+              }}
+              className="border p-1 rounded w-16"
+            />
+          </span>
+          <select
+            value={table.getState().pagination.pageSize}
+            onChange={e => {
+              table.setPageSize(Number(e.target.value))
+            }}
+          >
+            {[10, 20, 30, 40, 50].map(pageSize => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>{table.getPrePaginationRowModel().rows.length} Rows</div>
+        {/* <div>
+          <button onClick={() => rerender()}>Force Rerender</button>
+        </div>
+        <div>
+          <button onClick={() => refreshData()}>Refresh Data</button>
+        </div> */}
+        {/* {<pre>
+          {JSON.stringify(
+            {
+              columnFilters: table.getState().columnFilters,
+              globalFilter: table.getState().globalFilter
+            },
+            null,
+            2
+          )}
+        </pre>} */}
+      </div>
+
+    </main>
+  )
+}
+
+/* function Filter({ column }) {
+  const columnFilterValue = column.getFilterValue()
+
+  return (
+    <DebouncedInput
+      type="text"
+      value={columnFilterValue ?? ""}
+      onChange={value => column.setFilterValue(value)}
+      placeholder={`Search...`}
+      className="w-36 border shadow rounded"
+    />
+  )
+} */
+
+// A typical debounced input react component
+function DebouncedInput({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}) {
+  const [value, setValue] = React.useState(initialValue)
+
+  React.useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value)
+    }, debounce)
+
+    return () => clearTimeout(timeout)
+  }, [value])
+
+  return (
+    <input {...props} value={value} onChange={e => setValue(e.target.value)} />
+  )
+}
+
+/* const rootElement = document.getElementById("root")
+if (!rootElement) throw new Error("Failed to find the root element")
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+) */
+
+  /* function Employee() {
+  return (
+    <main>
+      <h1>Current Employees</h1>
       <NavLink to='/'>Home</NavLink>
     </main>
   );
-}
+} */
+
 
 export default Employee;
---
+++ src/pages/employee/makeData.js
@@ -0,0 +1,12 @@
+export function makeData(numberOfRows) {
+  // Code pour générer des données de test
+  const data = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    data.push({
+      /* firstName: `Prénom ${i}`,
+      lastName: `Nom ${i}`,
+      startDate: '2021-03-1' + i, */
+    });
+  }
+  return data;
+}
