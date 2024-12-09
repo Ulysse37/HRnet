c5ba1dcb2e015f6755a704ade086e2ac7bfcbb14
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-12-09 09:55:22 +0100

    style table page employée

--- src/pages/employee/Employee.jsx
+++ src/pages/employee/Employee.jsx
@@ -1,8 +1,10 @@
 import './employee.css';
 import { NavLink } from 'react-router-dom';
+import FlecheAsc from '../../assets/sort_asc.png';
+import FlecheDesc from '../../assets/sort_desc.png';
 
 import React from "react"
-import ReactDOM from "react-dom/client"
+/* import ReactDOM from "react-dom/client" */
 import {
   flexRender,
   getCoreRowModel,
@@ -63,6 +65,7 @@ function Employee() {
       }, */
       {
         accessorKey: "firstName",
+        header: "First Name",
         cell: info => info.getValue(),
         filterFn: "includesStringSensitive" //note: normal non-fuzzy filter column
       },
@@ -70,7 +73,7 @@ function Employee() {
         accessorFn: row => row.lastName, //note: normal non-fuzzy filter column - case sensitive
         id: "lastName",
         cell: info => info.getValue(),
-        header: () => <span>Last Name</span>,
+        header: "Last Name",
         filterFn: "includesString" //note: normal non-fuzzy filter column - case insensitive
       },
       {
@@ -149,25 +152,35 @@ function Employee() {
   return (
     <main>
       <h1>Current Employees</h1>
-      <NavLink to='/'>Home</NavLink>
-    
       <div className="p-2">
-        <div>
+        <div className='entries-search-position'>
+          <select
+              value={table.getState().pagination.pageSize}
+              onChange={e => {
+                table.setPageSize(Number(e.target.value))
+              }}
+            >
+              {[10, 25, 50, 100].map(pageSize => (
+                <option key={pageSize} value={pageSize}>
+                  Show {pageSize} entries
+                </option>
+              ))}
+            </select>
           <DebouncedInput
             value={globalFilter ?? ""}
             onChange={value => setGlobalFilter(String(value))}
             className="p-2 font-lg shadow border border-block"
-            placeholder="Search all columns..."
+            placeholder="Search"
           />
         </div>
         <div className="h-2" />
-        <table>
+        <table className="table-padding">
           <thead>
             {table.getHeaderGroups().map(headerGroup => (
               <tr key={headerGroup.id}>
                 {headerGroup.headers.map(header => {
                   return (
-                    <th key={header.id} colSpan={header.colSpan}>
+                    <th key={header.id} colSpan={header.colSpan} className='column-padding'>
                       {header.isPlaceholder ? null : (
                         <>
                           <div
@@ -182,16 +195,16 @@ function Employee() {
                               header.column.columnDef.header,
                               header.getContext()
                             )}
-                            {{
+                             {header.column.getIsSorted() === "asc" ? (
+                                <img src={FlecheAsc} alt="Asc" />
+                              ) : header.column.getIsSorted() === "desc" ? (
+                                <img src={FlecheDesc} alt="Desc" />
+                              ) : null}
+                            {/* {{
                               asc: " 🔼",
                               desc: " 🔽"
-                            }[header.column.getIsSorted()] ?? null}
+                            }[header.column.getIsSorted()] ?? null} */}
                           </div>
-                          {/* {header.column.getCanFilter() ? (
-                            <div>
-                              <Filter column={header.column} />
-                            </div>
-                          ) : null} */}
                         </>
                       )}
                     </th>
@@ -220,7 +233,14 @@ function Employee() {
           </tbody>
         </table>
         <div className="h-2" />
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 bottom-table-info">
+          <span className="flex items-center gap-1 page-number">
+            <div className='page-number-text'>Page</div>
+            <strong>
+              {table.getState().pagination.pageIndex + 1} of{" "}
+              {table.getPageCount()}
+            </strong>
+          </span>
           <button
             className="border rounded p-1"
             onClick={() => table.setPageIndex(0)}
@@ -249,74 +269,12 @@ function Employee() {
           >
             {">>"}
           </button>
-          <span className="flex items-center gap-1">
-            <div>Page</div>
-            <strong>
-              {table.getState().pagination.pageIndex + 1} of{" "}
-              {table.getPageCount()}
-            </strong>
-          </span>
-          <span className="flex items-center gap-1">
-            | Go to page:
-            <input
-              type="number"
-              defaultValue={table.getState().pagination.pageIndex + 1}
-              onChange={e => {
-                const page = e.target.value ? Number(e.target.value) - 1 : 0
-                table.setPageIndex(page)
-              }}
-              className="border p-1 rounded w-16"
-            />
-          </span>
-          <select
-            value={table.getState().pagination.pageSize}
-            onChange={e => {
-              table.setPageSize(Number(e.target.value))
-            }}
-          >
-            {[10, 20, 30, 40, 50].map(pageSize => (
-              <option key={pageSize} value={pageSize}>
-                Show {pageSize}
-              </option>
-            ))}
-          </select>
         </div>
-        <div>{table.getPrePaginationRowModel().rows.length} Rows</div>
-        {/* <div>
-          <button onClick={() => rerender()}>Force Rerender</button>
-        </div>
-        <div>
-          <button onClick={() => refreshData()}>Refresh Data</button>
-        </div> */}
-        {/* {<pre>
-          {JSON.stringify(
-            {
-              columnFilters: table.getState().columnFilters,
-              globalFilter: table.getState().globalFilter
-            },
-            null,
-            2
-          )}
-        </pre>} */}
       </div>
-
+      <NavLink to='/'>Home</NavLink>
     </main>
   )
 }
-
-/* function Filter({ column }) {
-  const columnFilterValue = column.getFilterValue()
-
-  return (
-    <DebouncedInput
-      type="text"
-      value={columnFilterValue ?? ""}
-      onChange={value => column.setFilterValue(value)}
-      placeholder={`Search...`}
-      className="w-36 border shadow rounded"
-    />
-  )
-} */
 
 // A typical debounced input react component
 function DebouncedInput({
@@ -343,24 +301,5 @@ function DebouncedInput({
     <input {...props} value={value} onChange={e => setValue(e.target.value)} />
   )
 }
-
-/* const rootElement = document.getElementById("root")
-if (!rootElement) throw new Error("Failed to find the root element")
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-) */
-
-  /* function Employee() {
-  return (
-    <main>
-      <h1>Current Employees</h1>
-      <NavLink to='/'>Home</NavLink>
-    </main>
-  );
-} */
-
 
 export default Employee;
--- src/pages/employee/makeData.js
+++ src/pages/employee/makeData.js
@@ -3,9 +3,15 @@ export function makeData(numberOfRows) {
   const data = [];
   for (let i = 0; i < numberOfRows; i++) {
     data.push({
-      /* firstName: `Prénom ${i}`,
+      firstName: `Prénom ${i}`,
       lastName: `Nom ${i}`,
-      startDate: '2021-03-1' + i, */
+      startDate: '2021-03-1' + i,
+      department: 'Filler text',
+      dateOfBirth: 'Filler text',
+      street: 'Filler text',
+      city: 'Filler text',
+      state:'Filler text',
+      zipCode: 'Filler text'
     });
   }
   return data;
