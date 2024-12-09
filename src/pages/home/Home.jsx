c471f9e7aafb86398a13950981399eb02f130e66
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-12-09 10:53:44 +0100

    stockage du formulaire dans un state

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,10 +14,43 @@ const { states, departments } = data;
 const stateOptions = states.map(state => state.name); // créer un tableau avec le nom des états pour le donner au dropdown
 const departmentOptions = departments.map(department => department.name); // De même pour les departments 
 
+
+
 function Home() {
-  const _onSelect = (option) => {
-    console.log(option.name); 
+
+  const [selectedStateOption, setSelectedStateOption] = useState(stateOptions[0]);
+  const [selectedDepartmentOption, setselectedDepartmentOption] = useState(departmentOptions[0]);
+  const _onSelectState = (option) => { // utilisé pour le dropdown
+    setSelectedStateOption(option.value);
+    /* console.log(option.name); */ 
   };
+  const _onSelectDepartment = (option) => { // utilisé pour le dropdown
+    setselectedDepartmentOption(option.value);
+    /* console.log(option.name); */ 
+  };
+
+
+  const [employees, setEmployees] = useState([]);
+
+  const handleSubmit = (event) => {
+    /* event.preventDefault(); */
+    const employee = {
+      firstName: event.target.elements.firstName.value,
+      lastName: event.target.elements.lastName.value,
+      birthDate: startDate1,
+      startDate: startDate2,
+      street: event.target.elements.street.value,
+      city: event.target.elements.city.value,
+      state: selectedStateOption,
+      zipCode: event.target.elements.zipCode.value,
+      department: selectedDepartmentOption
+      // ...
+    };
+    setEmployees([...employees, employee]);
+    console.log(employee);
+    console.log(employees);
+  };
+
 
   const [startDate1, setStartDate1] = useState(null);
   const [startDate2, setStartDate2] = useState(null);
@@ -29,35 +62,35 @@ function Home() {
           <NavLink to='/employee-list'>View Current Employees</NavLink>
         <h2>Create Employee</h2>
       </header>
-      <form action="#" id="create-employee">
+      <form action="#" id="create-employee" onSubmit={handleSubmit}>
         <ul className='main-ul'>
-          <TextField label="First Name" id="first-name" type="text" labelCLassName="name-textfield"/>
-          <TextField label="Last Name" id="last-name" type="text" labelCLassName="name-textfield"/>
+          <TextField label="First Name" htmlFor="firstName" id="firstName" type="text" labelCLassName="name-textfield"/>
+          <TextField label="Last Name" htmlFor="lastName" id="lastName" type="text" labelCLassName="name-textfield"/>
           <fieldset className='date-fieldset'>
             <legend className='date-fieldset-legend'>Date of Birth</legend>
-            <DatePicker selected={startDate1} onChange={(date) => setStartDate1(date)} />
+            <DatePicker selected={startDate1} onChange={(date) => setStartDate1(date)} value={startDate1} />
           </fieldset>
           <fieldset className='date-fieldset'>
             <legend className='date-fieldset-legend'>Start Date</legend>
-            <DatePicker selected={startDate2} onChange={(date) => setStartDate2(date)} />
+            <DatePicker selected={startDate2} onChange={(date) => setStartDate2(date)} value={startDate2} />
           </fieldset>
         </ul>
         <fieldset className="address">
           <legend>Address</legend>
           <ul>
-            <TextField label="Street" id="street" type="text" containerClassName="adress-list-elt"/>
-            <TextField label="City" id="city" type="text" containerClassName="adress-list-elt"/>
+            <TextField label="Street" htmlFor="street" id="street" type="text" containerClassName="adress-list-elt"/>
+            <TextField label="City" htmlFor ="city" id="city" type="text" containerClassName="adress-list-elt"/>
             <fieldset className='state-fieldset'>
               <legend>State</legend>
-              <Dropdown options={stateOptions} onChange={_onSelect} value={stateOptions[0]} placeholder="Select an option" />
+              <Dropdown options={stateOptions} onChange={_onSelectState} value={selectedStateOption} placeholder="Select an option" />
             </fieldset>
             {/* <SelectField label="State" name="state" id="state" options={states} />  */}       
-            <TextField label="Zip Code" id="zip-code" type="number" containerClassName="adress-list-elt"/>
+            <TextField label="Zip Code" htmlFor="zipCode" id="zipCode" type="number" containerClassName="adress-list-elt"/>
           </ul>   
         </fieldset>
         <fieldset className="department-fieldset">
           <legend>Department</legend>
-          <Dropdown className="department-select" options={departmentOptions} onChange={_onSelect} value={departmentOptions[0]} placeholder="Select an option" />
+          <Dropdown className="department-select" options={departmentOptions} onChange={_onSelectDepartment} value={selectedDepartmentOption} placeholder="Select an option" />
           {/* <SelectField label="Department" name="department" id="department" options={departments} containerClassName="department-select"/> */}
         </fieldset>  
         <button>Save</button>
